ef816719bee7eb5b8e04c7231806331b114ce03b
duypv98 <duypv98@gmail.com>
2021-01-19 19:17:36 +0700

    del data

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,11 +1,23 @@
 import JSONdb from 'simple-json-db';
 import path from 'path';
 import url from 'url';
+import fs from 'fs';
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
-const users = new JSONdb(`${__dirname}/data/users.json`);
-const notes = new JSONdb(`${__dirname}/data/notes.json`);
+const userDBPath = `${__dirname}/data/users.json`;
+const noteDBPath = `${__dirname}/data/notes.json`;
+
+export function initDB() {
+  [userDBPath, noteDBPath].forEach(dbPath => {
+    if (!fs.existsSync(dbPath)) {
+      fs.writeFileSync(dbPath, '{}');
+    }
+  });
+}
+
+const users = new JSONdb(userDBPath);
+const notes = new JSONdb(noteDBPath);
 
 export default {
   users,
--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 
 import apis from './apis/index.js';
 import { handleAPIError, handleInvalidRouteError } from './middlewares/errorHandlers.js'
+import { initDB } from './db/index.js';
 
 const PORT = process.env.PORT || 5001;
 
@@ -21,5 +22,6 @@ app.use(handleAPIError);
 app.use(handleInvalidRouteError);
 
 app.listen(PORT, () => {
+  initDB();
   console.log(`Server is running at http://localhost:${PORT}`);
 });
